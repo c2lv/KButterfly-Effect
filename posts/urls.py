42a9518fb5ee7c0dbfe4dbfb561e3d6013e6aa63
lhs961021 <lhs961021@naver.com>
2021-09-18 01:14:48 +0900

    20210918 link

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from .views import *
 from . import views
 
 app_name = 'posts'
 urlpatterns = [
+    path("about/",about, name="about"),
+    path("blog_single/",blog_single, name="blog_single"),
+    path("categories/",categories, name="categories"),
+    path("contact/",contact, name="contact"),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about(request):
+    return render(request, "posts/about.html")
+
+
+def blog_single(request):
+    return render(request, "posts/blog-single.html")
+
+
+def categories(request):
+    return render(request, "posts/categories.html")
+
+
+def contact(request):
+    return render(request, "posts/contact.html")
