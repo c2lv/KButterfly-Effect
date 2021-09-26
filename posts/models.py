84aea642f40af74a3b26188853e99aee234bf74f
LeeTaeGeom <taegeom0601@gmail.com>
2021-09-26 19:57:47 +0900

    postlist 수정

--- posts/models.py
+++ posts/models.py
@@ -7,10 +7,21 @@ class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     title_tag = models.CharField(max_length=20) # 요약 태그
-    hashtag = models.CharField(max_length=30)  #해시 태그, 여러개하면 할때마다 띄워쓰기 시키기
+    hashtag = models.TextField(max_length=30)  #해시 태그, 여러개하면 할때마다 띄워쓰기 시키기
     eco_score = models.IntegerField(default=0) #eco score
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now=True)
     deadline = models.DateField(auto_now=False)
     body = models.TextField()
     image = models.ImageField(upload_to="post/",null=True, blank=True)
+
+
+#좋아요 모델
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together =(('user', 'post'))
