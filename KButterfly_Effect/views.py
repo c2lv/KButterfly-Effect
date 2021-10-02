7acc63d1f0a75f7579852675d47896773e9834c0
c2lv <hyeonjun7@naver.com>
2021-10-02 22:18:22 +0900

    상단 캐러셀 Post 랜덤 출력 구현

--- KButterfly_Effect/views.py
+++ KButterfly_Effect/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from posts.models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    a = list(Post.objects.all().order_by("?"))
+    b = a[0]
+    c = a[2]
+    context = {
+        "b":b,
+        "c":c,
+    }
+    return render(request, "index.html", context)
 
 def about(request):
     return render(request, "main/about.html")
