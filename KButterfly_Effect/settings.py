6cea6a5ea4ddafc6ead5466ad447d8d6b74e06a5
c2lv <hyeonjun7@naver.com>
2021-10-03 02:27:21 +0900

    final check

--- KButterfly_Effect/settings.py
+++ KButterfly_Effect/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -158,3 +159,6 @@ ACCOUNT_FORMS = {'signup': 'users.forms.SignupForm'}
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
